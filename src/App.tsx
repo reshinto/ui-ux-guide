231a59eebd6ad9cb5dc0bf8cc01d5a15055c675b
reshinto <terencekong2002@gmail.com>
2021-12-05 00:50:22 +0800

    Add expo cached to improve loading performance

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import {StyleSheet, StatusBar} from "react-native";
 import {colors} from "./shared/styles/palette";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import Navigation from "./shared/components/Navigation";
+import useCachedResources from "./shared/hooks/useCachedResources";
 
 export default function App() {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
